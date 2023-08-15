82509eb944d114a764e63b20d4dbf2b875dce659
hi2102 <hi2102@naver.com>
2023-08-15 21:25:05 +0900

    TEST: Add Test Codes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <>initial page</>;
+import Link from 'next/link';
+
+export default function Page() {
+	return (
+		<nav className='flex items-center'>
+			<Link href='/look-up'>
+				<button className='border-none bg-slate-400 text-slate-100 rounded-lg px-4 py-2 m-4 hover:cursor-pointer hover:bg-slate-700'>
+					회원가입
+				</button>
+			</Link>
+		</nav>
+	);
 }
