c358dbce511489243cb8fcd6ffb64925ceb53222
hi2102 <hi2102@naver.com>
2023-08-15 21:24:03 +0900

    CHORE : Edit Pakage Files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	experimental: {
+		images: {
+			allowFutureImage: true,
+		},
+		serverActions: true,
+	},
+	pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
+};
 
 module.exports = nextConfig
