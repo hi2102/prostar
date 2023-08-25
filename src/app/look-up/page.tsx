51fb15ebf5e070d53367aefe57bc50e208756a00
hi2102 <hi2102@naver.com>
2023-08-25 12:05:08 +0900

    DOCS : Edit Look-up Page

--- src/app/look-up/page.tsx
+++ src/app/look-up/page.tsx
@@ -4,6 +4,8 @@ import { useState, ChangeEvent, FormEvent } from 'react';
 import { useRouter } from 'next/navigation';
 import Spinner from '../../../public/assets/spinner.svg';
 
+import { LoginButton } from '../components/nextAuthButton';
+
 interface FormState {
 	state: boolean;
 	inputValue: string;
@@ -49,38 +51,45 @@ export default function SignIn() {
 	}
 
 	return (
-		<form
-			action={formState.state === true ? 'look-up/join' : ''}
-			onSubmit={handleSubmit}>
-			<h1 className='text-[28px] leading-8 py-3 mb-6'>
-				가입 또는 로그인을 위해 이메일을 입력하세요.
-			</h1>
-			<input
-				required
-				type='email'
-				placeholder='이메일'
-				className='border border-slate-500 w-full px-2 py-4 rounded-lg my-2 mb-14'
-				onChange={handleChange}
-			/>
-			<p className='text-neutral-400 '>
-				계속 진행하면 프로스타짐의{' '}
-				<b className='underline underline-offset-auto'>개인정보 처리방침</b> 및{' '}
-				<b className='underline underline-offset-auto'>이용약관</b>에 동의하게
-				됩니다.
-			</p>
-			<button className='float-right mt-16 bg-black text-white px-7 py-3 rounded-full'>
-				{!formState.loading ? (
-					'계속'
-				) : (
-					// eslint-disable-next-line @next/next/no-img-element
-					<img
-						src={Spinner.src}
-						width={28}
-						alt='loading spinner'
-						className='animate-spin disabled:opacity-75'
-					/>
-				)}
-			</button>
-		</form>
+		<div className='mt-20 mb-20 mx-auto w-[32.75rem] px-8'>
+			<form
+				action={formState.state === true ? 'look-up/join' : ''}
+				onSubmit={handleSubmit}>
+				<h1 className='text-[28px] leading-8 py-3 mb-6'>
+					가입 또는 로그인을 위해 이메일을 입력하세요.
+				</h1>
+				<input
+					required
+					type='email'
+					placeholder='이메일'
+					className='border border-slate-500 w-full px-2 py-4 rounded-lg my-2 mb-14'
+					onChange={handleChange}
+				/>
+				<p className='text-neutral-400 '>
+					계속 진행하면 프로스타짐의{' '}
+					<b className='underline underline-offset-auto'>개인정보 처리방침</b>{' '}
+					및 <b className='underline underline-offset-auto'>이용약관</b>에
+					동의하게 됩니다.
+				</p>
+				<div className='flex justify-end mt-20'>
+					<button className=' bg-black text-white px-7 py-3 rounded-full'>
+						{!formState.loading ? (
+							'계속'
+						) : (
+							// eslint-disable-next-line @next/next/no-img-element
+							<img
+								src={Spinner.src}
+								width={28}
+								alt='loading spinner'
+								className='animate-spin disabled:opacity-75'
+							/>
+						)}
+					</button>
+				</div>
+			</form>
+			<section className='my-20'>
+				<LoginButton />
+			</section>
+		</div>
 	);
 }
